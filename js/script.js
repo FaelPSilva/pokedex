2e16d6b1c1c23120129f499fd19cc406258a5cf2
FaelPSilva <164876697+FaelPSilva@users.noreply.github.com>
2025-01-28 15:05:26 -0300

    atualização consulta api

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImg = document.querySelector('.pokemon_img');
 const pokemonType = document.querySelector('#spec');
 const pokemonAbilities = document.querySelector("#skills");
+const pokemonEvolution = document.querySelector("#evo");
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
@@ -27,6 +28,26 @@ const fetchPokemon = async(pokemon) =>{
 
 }
 
+
+const fetchEvolutionChain = async (speciesUrl) => {
+    // A URL de species fornece a URL da cadeia evolutiva
+    const speciesResponse = await fetch(speciesUrl);
+    const speciesData = await speciesResponse.json();
+    const evolutionUrl = speciesData.evolution_chain.url;
+
+    
+
+    // Agora pegamos a URL da cadeia de evolução
+    const evolutionResponse = await fetch(evolutionUrl);
+    const evolutionData = await evolutionResponse.json();
+    
+    return evolutionData;
+
+    
+};
+
+
+
 const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
@@ -34,16 +55,35 @@ const renderPokemon = async (pokemon) => {
     if (data){
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
-        pokemonAbilities.innerHTML = data['abilities']['1']['ability']['name'];
+        pokemonAbilities.innerHTML = data['abilities']['0']['ability']['name'];
         pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
         pokemonType.innerHTML = data['types']['0']['type']['name']
+
+        const evolutionData = await fetchEvolutionChain(data.species.url);
+        const evolutionChain = [];
+        let currentEvolution = evolutionData.chain;
+
+        // Extraindo as evoluções da cadeia
+        while (currentEvolution) {
+            evolutionChain.push(currentEvolution.species.name);
+            currentEvolution = currentEvolution.evolves_to ? currentEvolution.evolves_to[0] : null;
+        }
+
+        // Exibindo a cadeia evolutiva
+        pokemonEvolution.innerHTML = ` ${evolutionChain.join(' → ')}`;
+
+
+
         input.value = '';
         searchPokemon = data.id;
 
     } else {
         pokemonName.innerHTML = 'Não Encontrado'
+
         pokemonNumber.innerHTML = '';
         pokemonImg.src = '';
+        pokemonAbilities.innerHTML = 'Não Informado';
+        pokemonEvolution.innerHTML = 'Não Informado';
         input.value = '';
     }   
 }
